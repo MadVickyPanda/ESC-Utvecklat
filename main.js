65971cb26f0e8100ba4a94debbc62fbd3c7f71a3
MadVickyPanda <solboll@hotmail.com>
2025-11-07 14:47:25 +0100

    format documents, fix animations and change size logo and header text

--- main.js
+++ main.js
@@ -1,27 +1,21 @@
 const hamburger = document.getElementById('hamburger');
-const popupMenu = document.getElementById('popup-menu');
-const closeBtn = document.getElementById('close-btn');
+const popupMenu = document.getElementById('popupMenu');
+const closeBtn = document.getElementById('closeBtn');
 const overlay = document.getElementById('overlay');
 
 hamburger.addEventListener('click', () => {
-  // 1️⃣ Fade the page first
   overlay.classList.add('active');
-
-  // 2️⃣ Show the menu after a short delay
   setTimeout(() => {
     popupMenu.classList.add('active');
-  }, 90); // 200ms delay
+  }, 90);
 });
 
 closeBtn.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
 
 function closeMenu() {
-  // Hide menu first
   popupMenu.classList.remove('active');
-
-  // Then fade the page back in
   setTimeout(() => {
     overlay.classList.remove('active');
-  }, 200); // match the delay
+  }, 90); 
 }
