98dcabc3e317da18a932bcc47680b5314558543a
Batyah <iman.nagati@lerniastudent.se>
2025-11-21 23:54:11 +0100

    Rensat API och features-sida, bara kvar min booking-del

--- API.js
+++ API.js
@@ -1,9 +1,17 @@
+const API_URL = "https://lernia-sjj-assignments.vercel.app/api/challenges";
+
 export async function fetchChallenges() {
-  const response = await fetch(
-    "https://lernia-sjj-assignments.vercel.app/api/challenges"
-  );
+  const response = await fetch(API_URL);
+
   if (!response.ok) {
-    throw new Error("Failed to fetch challenges");
+    throw new Error("Network response was not ok");
   }
+
   const data = await response.json();
-  return data;
+
+  //console.loggar bara för eran skull om det är något ni vill se
+  console.log(data.challenges);
+  console.log(data);
+  // API:t returnerar { challenges: [...] }
+  return data.challenges; // <-- returnerar arrayen direkt
+}
